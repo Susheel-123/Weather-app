330d235de97fb719e7f4769da5b6373e74376b71
Susheel singh <susheelsinghajay@gmail.com>
2023-08-27 21:06:06 +0530

    weather app

---
+++ rock.js
@@ -0,0 +1,13 @@
+const apiKey = "6a152e32fe645c157b90915054ccf32c";
+
+const weatherDataE1 = document.getElementById("weather-data");
+
+const cityInputE1 = document.getElementById("city-input");
+
+const formE1 = document.querySelector("form");
+
+formE1.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputE1.value;
+    console.log(cityValue);
+ });
